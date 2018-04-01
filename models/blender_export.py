d8620ad55f308837ca16e372d789f7db8568c03c
freds <frederic.souchu@laposte.net>
2018-04-01 23:08:34 +0200

    reading models from map

--- models/blender_export.py
+++ models/blender_export.py
@@ -1,62 +1,49 @@
 import bpy
 import bmesh
-
-s = "\"{}\":{{\n".format(bpy.context.object.name)
+import re
 
 obdata = bpy.context.object.data
+
+# charset
+charset="_0123456789abcdefghijklmnopqrstuvwxyz"
+
+# how many models (to be manually edited)
+s = ""
+
+# object name
+name = bpy.context.object.name.lower()
+s = s + "{:02x}".format(len(name))
+for c in name:
+    s = s + "{:02x}".format(charset.index(c)+1)
+
 bm = bmesh.new()
 bm.from_mesh(obdata)
 
-s = s + "\"v\":["
-first=True
+s = s + "{:02x}".format(len(obdata.vertices))
 for v in obdata.vertices:
-    if first==False:
-        s = s + ','
-    s = s + "[{},{},{}]".format(round(v.co.x,2), round(v.co.z,2), round(v.co.y,2))
-    first=False
-
-s = s + "],\n"
+    s = s + "{:02x}{:02x}{:02x}".format(int(round(32*v.co.x+128,0)), int(round(32*v.co.z+128,0)), int(round(32*v.co.y+128,0)))
 
 # faces:
-# edge count
-# edge id
-s = s + "\"f\":["
-first=True
+s = s + "{:02x}".format(len(bm.faces))
 for f in bm.faces:
-    if first==False:
-        s = s + ","
-    # number of edges
-    # point index in face
-    s = s + "[{},{}".format(len(f.edges),f.verts[0].index+1)
-    # all edges
+    # face point index
+    # edge count
+    s = s + "{:02x}{:02x}".format(f.verts[0].index+1,len(f.edges))
+    # edge id's
     for e in f.edges:
-        s = s + ",{}".format(e.index+1)
-    s = s + "]"
-    first=False
-
-s = s + "],\n"
+        s = s + "{:02x}".format(e.index+1)
 
 #normals
-s = s + "\"n\":["
-first=True
+s = s + "{:02x}".format(len(obdata.polygons))
 for f in obdata.polygons:
-    if first==False:
-        s = s + ","
-    s = s + "[{},{},{}]".format(round(f.normal.x,2), round(f.normal.z,2), round(f.normal.y,2))
-    first=False
-
-s = s + "],\n"
+    s = s + "{:02x}{:02x}{:02x}".format(int(round(32*f.normal.x+128,0)), int(round(32*f.normal.z+128,0)), int(round(32*f.normal.y+128,0)))
 
 # all edges
-s = s + "\"e\":["
-first=True
+s = s + "{:02x}".format(len(bm.edges))
 for e in bm.edges:
-    if first==False:
-        s = s + ","
-    s = s + "[{},{},{}]".format(e.verts[0].index+1, e.verts[1].index+1,'true' if e.is_wire else '-1')
-    first=False
+    s = s + "{:02x}{:02x}{:02x}".format(e.verts[0].index+1, e.verts[1].index+1,1 if e.is_wire else 0)
 
-s = s + "]}\n"
+#s = re.sub("(.{256})", "\\1\n", s, 0, re.DOTALL)
 print(s)
 
 # copy to clipboard
